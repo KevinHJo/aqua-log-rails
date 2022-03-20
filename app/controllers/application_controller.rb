e98e4b38ab1eaad3ea164592ba9f69317cb9fad7
Kevin Jo <kevinhjo725@gmail.com>
2022-03-20 14:00:46 -0700

    Create sessions and application controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,46 @@
 class ApplicationController < ActionController::Base
+  before_action :underscore_params!
+  helper_method :logged_in?, :current_user
+
+  #CLLLR
+  def current_user
+    return nil if session[:session_token].nil?
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout!
+    session[:session_token] = nil
+    @current_user.reset_session_token!
+    @current_user = nil
+  end
+
+  def require_logged_in
+    render json: ['Please login to continue'] unless logged_in?
+  end
+
+  def underscore_params!
+    underscore_hash = -> (hash) do
+    hash.transform_keys!(&:underscore)
+    hash.each do |key, value|
+      if value.is_a?(ActionController::Parameters)
+      underscore_hash.call(value)
+      elsif value.is_a?(Array)
+      value.each do |item|
+        next unless item.is_a?(ActionController::Parameters)
+        underscore_hash.call(item)
+      end
+      end
+    end
+    end
+    underscore_hash.call(params)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email          (email) UNIQUE
+#  index_users_on_session_token  (session_token) UNIQUE
+#  index_users_on_username       (username) UNIQUE
+#
 class User < ApplicationRecord
-  
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.is_password?(password)
+      return @user
+    else
+      return nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    return self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end
