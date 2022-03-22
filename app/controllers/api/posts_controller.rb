dd3b3285c0a42462a3dd4772fa96574f470f2004
Kevin Jo <kevinhjo725@gmail.com>
2022-03-21 20:52:34 -0700

    Create posts controller with index and create methods

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,3 +1,21 @@
 class Api::PostsController < ApplicationController
-  
+  def index
+    @posts = Post.all
+    render 'api/posts/index'
+  end
+
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      render 'api/posts/show'
+    else
+      render json: @post.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body, :author_id, :parent_id)
+  end
 end
