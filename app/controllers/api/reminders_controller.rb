bc93e64274b0df3789a51196e1db9d9b7ace9d8f
Kevin Jo <kevinhjo725@gmail.com>
2022-04-14 14:04:32 -0700

    Begin creation of reminders

--- app/controllers/api/reminders_controller.rb
+++ app/controllers/api/reminders_controller.rb
@@ -1,8 +1,5 @@
 class Api::RemindersController < ApplicationController
   def index
-    # user = User.find(params[:user_id])
-    # @reminders = user.reminders
-
     @reminders = Reminder.where({owner_id: params[:user_id]})
     render 'api/reminders/index'
   end
